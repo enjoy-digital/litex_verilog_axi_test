292187b9bf08f3e4db9e6c6858b3f527d413829a
Florent Kermarrec <florent@enjoy-digital.fr>
2022-06-07 15:18:51 +0200

    Add minimal sim for testing AXI-Lite.

---
+++ test_axi_lite.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+#
+# This file is part of LiteX-Verilog-AXI-Test
+#
+# Copyright (c) 2022 Florent Kermarrec <florent@enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+import argparse
+
+from migen import *
+
+from litex.build.generic_platform import *
+from litex.build.sim import SimPlatform
+from litex.build.sim.config import SimConfig
+from litex.build.sim.verilator import verilator_build_args, verilator_build_argdict
+
+from litex.soc.interconnect.csr import *
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.soc import SoCRegion
+from litex.soc.integration.builder import *
+from litex.soc.interconnect.axi import *
+
+from verilog_axi.axi_common import *
+
+# IOs ----------------------------------------------------------------------------------------------
+
+_io = [
+    # Clk / Rst.
+    ("sys_clk", 0, Pins(1)),
+    ("sys_rst", 0, Pins(1)),
+
+    # Serial.
+    ("serial", 0,
+        Subsignal("source_valid", Pins(1)),
+        Subsignal("source_ready", Pins(1)),
+        Subsignal("source_data",  Pins(8)),
+
+        Subsignal("sink_valid", Pins(1)),
+        Subsignal("sink_ready", Pins(1)),
+        Subsignal("sink_data",  Pins(8)),
+    ),
+]
+
+# Platform -----------------------------------------------------------------------------------------
+
+class Platform(SimPlatform):
+    name = "sim"
+    def __init__(self):
+        SimPlatform.__init__(self, "SIM", _io)
+
+# AXISimSoC -------------------------------------------------------------------------------------------
+
+class AXISimSoC(SoCCore):
+    def __init__(self):
+        # Parameters.
+        sys_clk_freq = int(100e6)
+
+        # Platform.
+        platform     = Platform()
+        self.comb += platform.trace.eq(1)
+
+        # CRG --------------------------------------------------------------------------------------
+        self.submodules.crg = CRG(platform.request("sys_clk"))
+
+        # SoCCore ----------------------------------------------------------------------------------
+        SoCCore.__init__(self, platform, clk_freq=sys_clk_freq, bus_standard="axi-lite", uart_name="sim", integrated_rom_size=0x10000)
+        self.add_config("BIOS_NO_BOOT")
+
+        # AXI-Lite Tests ---------------------------------------------------------------------------
+        def axi_syntax_test():
+            pass
+
+        def axi_integration_test():
+            pass
+
+        axi_syntax_test()
+        axi_integration_test()
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    parser = argparse.ArgumentParser(description="LiteX Verilog AXI test simulation SoC ")
+    verilator_build_args(parser)
+    args = parser.parse_args()
+    verilator_build_kwargs = verilator_build_argdict(args)
+    sim_config = SimConfig(default_clk="sys_clk")
+    sim_config.add_module("serial2console", "serial")
+
+    soc = AXISimSoC()
+    builder = Builder(soc)
+    builder.build(sim_config=sim_config, **verilator_build_kwargs)
+
+if __name__ == "__main__":
+    main()
